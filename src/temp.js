3ee0a8835aade441eddd3abc432db2ccc706c99f
Akshay Patel <arp9975@gmail.com>
2023-07-29 11:05:24 -0300

    Removing the stuff

--- src/temp.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const fetchData = async () => {
-const response =  await axios.get(`https://2xi9f2w3e2.execute-api.us-east-1.amazonaws.com/dev`);
-
-console.log("Data received:", response);
-
-}
-fetchData();
